0b0ccb0eb78a88bf12d31d5ff784fdcf9b204841
HammadSheikh-2000 <hammadsheikh66@gmail.com>
2023-09-06 10:31:57 +0500

    Update FavouriteCard.js

--- components/SubComponents/FavouriteCard.js
+++ components/SubComponents/FavouriteCard.js
@@ -3,7 +3,6 @@ import { View,Text,StyleSheet,Dimensions,ActivityIndicator,ImageBackground} from
 import { useState } from "react";
 import { bg } from "../../assets/backgrounds";
 const API_Key="a6eca899a628a79d89f4879f54cd23a3";
-
 const FavouriteCard = (props) => {
 
     const[WeatherData,setWeatherData]=useState(null);
